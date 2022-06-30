f6a1468499f78c511f99616f7bdd654edc0ea631
artem <goldkuka@gmail.com>
2022-06-30 12:49:23 +0700

    2qest

---
+++ src/BonusMilesService.java
@@ -0,0 +1,17 @@
+import calculate.calculate;
+
+public class BonusMilesService {
+
+        public int calculate(int calcilate) {
+
+
+            int price = 10000;
+            int ticket = price;
+            int bonus = price / 20;
+            return bonus;
+
+
+        }
+
+}
+
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 10000;
-        int bonus = ticketPrice / 20;
-
-        System.out.println("Сумма купленного билета: " + ticketPrice);
-        System.out.println("Ваши бонусы: " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
 
     }
 }
-
