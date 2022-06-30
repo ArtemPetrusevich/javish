6bda9c6579d2633882eb2f61fcaeb93bdc10b2db
artem <goldkuka@gmail.com>
2022-06-30 12:53:37 +0700

    2qestu

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,16 +2,16 @@ import calculate.calculate;
 
 public class BonusMilesService {
 
-        public int calculate(int calcilate) {
+    public int calculate(int calcilate) {
 
 
-            int price = 10000;
-            int ticket = price;
-            int bonus = price / 20;
-            return bonus;
+        int price = 10000;
+        int ticket = price;
+        int bonus = price / 20;
+        return bonus;
 
 
-        }
+    }
 
 }
 
